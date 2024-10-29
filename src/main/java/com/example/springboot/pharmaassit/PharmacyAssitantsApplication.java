996e455bacd835fabb7ab8e70d7729753463a9c6
varadaraju17 <varada883@gmail.com>
2024-10-29 12:30:34 +0530

    Project initiating

---
+++ src/main/java/com/example/springboot/pharmaassit/PharmacyAssitantsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.pharmaassit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyAssitantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyAssitantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/pharmaassit/PharmacyAssitantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.pharmaassit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyAssitantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
